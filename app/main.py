270899cbac75513ab354c638cb1fb77788983328
kjw <yoonlight12@gmail.com>
2022-08-18 13:43:14 +0900

    feat: add temporal fitbit auth

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.oauth import OAuth2Server
 from pydantic import BaseModel
 from fitbit import Fitbit
-
+from fastapi.responses import RedirectResponse
 from app.routers import items
 from app.config import REDIRECT_URI
 
@@ -28,9 +28,24 @@ def read_root():
 
 @app.post("/api/fitbit/login")
 def login_fitbit(fitbit_id: FitbitID):
-    server = OAuth2Server(fitbit_id.client_id, fitbit_id.client_secret, REDIRECT_URI)
+    server = OAuth2Server(fitbit_id.client_id,
+                          fitbit_id.client_secret, REDIRECT_URI)
     server.browser_authorize()
     return server.fitbit.client.session.token.items()
+
+
+@app.get("/fitbit")
+def fitbit(client_id: str, client_secret: str):
+    fitbit = Fitbit(client_id, client_secret, redirect_uri=REDIRECT_URI)
+    url, _ = fitbit.client.authorize_token_url()
+    return RedirectResponse(url)
+
+
+@app.get("/fitbit/auth")
+def fitbit_auth(client_id: str, client_secret: str, code: str):
+    fitbit = Fitbit(client_id, client_secret, redirect_uri=REDIRECT_URI)
+    fitbit.client.fetch_access_token(code)
+    return fitbit.client.session.token.items()
 
 
 @app.post("/api/fitbit/heart")
